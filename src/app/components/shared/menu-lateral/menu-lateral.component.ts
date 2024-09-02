5eb8fc4965945551de424b89bf806b685fefddae
Rafaela <rcfbritto@gmail.com>
2024-09-02 19:49:30 -0300

    Aplicação da identidade visual no sistema web

--- src/app/components/shared/menu-lateral/menu-lateral.component.ts
+++ src/app/components/shared/menu-lateral/menu-lateral.component.ts
@@ -10,15 +10,6 @@ import { MatSidenav } from '@angular/material/sidenav';
 })
 export class MenuLateralComponent {
 
-  @ViewChild(MatSidenav)
-  sidenav!: MatSidenav;
-  
-  isMobile= true;
-
-  isCollapsed = true;
-
-  constructor(private observer: BreakpointObserver) {}
-
   isOpen = false;
 
   openMenu() {
@@ -28,25 +19,4 @@ export class MenuLateralComponent {
   closeMenu() {
     this.isOpen = false;
   }
-
-  ngOnInit(): void {
-    this.observer.observe(['(max-width: 800px)']).subscribe((screenSize) => {
-      if(screenSize.matches){
-        this.isMobile = true;
-      } else {
-        this.isMobile = false;
-      }
-    });
-  }
-
-  toggleMenu() {
-    if(this.isMobile){
-      this.sidenav.toggle();
-      this.isCollapsed = false;
-    } else {
-      this.sidenav.open();
-      this.isCollapsed = !this.isCollapsed;
-    }
-  }
-
 }
