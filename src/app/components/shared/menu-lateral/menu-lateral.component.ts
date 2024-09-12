e3db54deb71772eaafd95f0e8ae576faa5b33177
Rafaela <rcfbritto@gmail.com>
2024-09-12 19:16:52 -0300

    Ajustes e correções na identidade visual do site

--- src/app/components/shared/menu-lateral/menu-lateral.component.ts
+++ src/app/components/shared/menu-lateral/menu-lateral.component.ts
@@ -1,6 +1,4 @@
-import { BreakpointObserver } from '@angular/cdk/layout';
 import { Component, Input, ViewChild } from '@angular/core';
-import { MatSidenav } from '@angular/material/sidenav';
 
 
 @Component({
@@ -12,11 +10,8 @@ export class MenuLateralComponent {
 
   isOpen = false;
 
-  openMenu() {
-    this.isOpen = true;
-  }
-
-  closeMenu() {
-    this.isOpen = false;
+  toggleMenu() {
+    this.isOpen = !this.isOpen; 
   }
 }
+
